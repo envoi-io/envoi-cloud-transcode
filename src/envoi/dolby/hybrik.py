b0e07c6f1e9c0e11bffefd56f3037fe4e5d11d43
John Whitson <john.whitson@gmail.com>
2024-03-29 12:10:17 -0600

    Fixed Hybrik job task retry delay_sec attribute name

--- src/envoi/dolby/hybrik.py
+++ src/envoi/dolby/hybrik.py
@@ -80,7 +80,7 @@ class HybrikApiClient(HttpClient):
         if task_retry_count:
             task_retry["count"] = task_retry_count
         if task_retry_delay_secs:
-            task_retry["delay_secs"] = task_retry_delay_secs
+            task_retry["delay_sec"] = task_retry_delay_secs
         body["task_retry"] = task_retry
 
         return self.call_api("post", endpoint, body=body)
